de069464e114a87b31c8fe11a9a4950a35589c64
Kevin Phan <90020140+KevinFPhan@users.noreply.github.com>
2022-01-20 22:05:48 -0800

    Enter

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'hlblue': '#020C6A',
+      },
+      height: {
+        '8h': '800px',
+        '285': '285px',
+        '7h': '700px',
+        '5h5': '550px',
+      },
+      width: {     
+        '560': '560px',
+      }
+    },
+  },
+  plugins: [],
+}
